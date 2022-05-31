fb6ae6033c757add45a79dde606e78fc9cc81b0e
unknown <ceinnova01374@innovaccion.mx>
2022-05-30 21:01:12 -0500

    initial commit

---
+++ Practica1.py
@@ -0,0 +1,23 @@
+calificacion = input("Ingresa tu calificaion de AZ-900: ")
+calificacion = int(calificacion)
+if calificacion < 700: 
+    print("Vees, por no estudiar")
+elif calificacion > 1000 :
+    print("Mientes, no puedes sacar mas de 1000")
+else : 
+    print("Felicidades pasa por tu certificado")
+
+edad = input("Ingresa tu edad: ")
+edad = int(edad)
+
+if edad >= 18 and edad <= 100 :
+    print("Eres mayor de edad")
+elif edad > 100 : 
+    print("Si vienes acompañad@ de tus abuelitos, si te podemos fiar")
+else : 
+    print("Eres menor de edad")
+
+
+
+    
+
